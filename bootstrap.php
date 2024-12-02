8ba32cdbd9960b6198dea0cfe682275b8fbf5c02
Fien Wouters <r0937053@student.thomasmore.be>
2024-12-02 09:44:05 +0100

    refactor: bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,6 @@ use Fienwouters\Onlinestore\Db;
 session_start();
 
 // Foutmeldingen inschakelen
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
